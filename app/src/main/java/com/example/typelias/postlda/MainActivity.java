808ada9384d0fe082172f8f41485297d85c42bd8
Typelias <elbe1703@student.miun.se>
2018-04-05 16:46:49 +0200

    REEEEEEEEEEEEEEEEEEE

--- app/src/main/java/com/example/typelias/postlda/MainActivity.java
+++ app/src/main/java/com/example/typelias/postlda/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.typelias.postlda;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     public Button but1;
+
+    public TextView owner;
+    public TextView adress;
+    public TextView zipcode;
+    public TextView mailName;
+    public TextView mailStatus;
+
+    SharedPreferences data;
+    static public String filename = "info";
 
     public void init() {
         but1 = (Button) findViewById(R.id.next);
@@ -17,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent toy = new Intent(MainActivity.this, settings.class);
+
 
                 startActivity(toy);
             }
@@ -30,7 +42,44 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        owner = (TextView)findViewById(R.id.owner);
+        adress = (TextView)findViewById(R.id.adress);
+        zipcode = (TextView)findViewById(R.id.zipcode);
+        mailName = (TextView)findViewById(R.id.mailName);
+
         init();
+        data = getSharedPreferences(filename,0);
+        String loadName = data.getString("name","Om inget visas klicka på setup");
+        owner.setText(loadName);
+        String loadAdress = data.getString("adress","");
+        adress.setText(loadAdress);
+        String loadZip = data.getString("zip","");
+        zipcode.setText(loadZip);
+        String loadMailName = data.getString("mailname","");
+        mailName.setText(loadMailName);
+        getSQLData();
+
+
     }
+
+    public void getSQLData()
+    {
+        mailStatus = (TextView)findViewById(R.id.mailStatus);
+        data = getSharedPreferences(filename,0);
+        int id = data.getInt("id",-2);
+        if(id == -2)
+        {
+            mailStatus.setText("Error. \nKlicka på setup\neller så är det fel id");
+        }
+        else
+        {
+
+
+        }
+
+    }
+
+
 
 }
--- app/src/main/java/com/example/typelias/postlda/settings.java
+++ app/src/main/java/com/example/typelias/postlda/settings.java
@@ -1,14 +1,73 @@
 package com.example.typelias.postlda;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class settings extends AppCompatActivity {
+
+    public Button submit;
+    public EditText fullname;
+    public EditText adres;
+    public EditText zip;
+    public EditText mail_id;
+    public EditText mail_name;
+
+    static public String filename = "info";
+    SharedPreferences data;
+
+
+    public String name;
+    public String adress;
+    public String zipcode;
+    public String id;
+    public String mailName;
+
+    public void save()
+    {
+        submit = (Button)findViewById(R.id.data_submit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //getting edit text from settings
+                fullname = (EditText) findViewById(R.id.fullname);
+                adres = (EditText) findViewById(R.id.adress);
+                zip = (EditText) findViewById(R.id.zip);
+                mail_id = (EditText) findViewById(R.id.mail_id);
+                mail_name = (EditText) findViewById(R.id.mail_name);
+
+
+                name = fullname.getText().toString();
+                adress = adres.getText().toString();
+                zipcode = zip.getText().toString();
+                id = mail_id.getText().toString();
+                mailName = mail_name.getText().toString();
+
+                int intID = Integer.parseInt(id);
+
+                SharedPreferences.Editor editor = data.edit();
+                editor.putString("name",name);
+                editor.putString("adress",adress);
+                editor.putString("zip",zipcode);
+                editor.putString("mailname",mailName);
+                editor.putInt("id",intID);
+                editor.commit();
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        save();
+
+        data = getSharedPreferences(filename,0);
     }
+
+
 }
